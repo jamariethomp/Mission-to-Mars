36fa37ffd646321ee804420a6d7c97dc96c88439
Jamie Thompson <jamariethomp@gmail.com>
2022-02-26 20:40:21 -0700

    Finalize deliverable 2. jmt

--- scraping.py
+++ scraping.py
@@ -19,7 +19,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "hemispheres": hemisphere_images(browser)
     }
 
     # Stop webdriver and return data
@@ -96,6 +96,30 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def hemisphere_images(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    hemisphere_image_urls = []
+
+    links = browser.find_by_css('a.product-item img')
+
+    for i in range(len(links)):
+            hemisphere = {}
+            
+            browser.find_by_css('a.product-item img')[i].click()
+                
+            sample_elem = browser.links.find_by_text('Sample').first
+            hemisphere['img_url'] = sample_elem['href']
+            
+            hemisphere['title'] = browser.find_by_css('h2.title').text
+            
+            hemisphere_image_urls.append(hemisphere)
+            
+            browser.back()
+
+    return hemisphere_image_urls            
 
 if __name__ == "__main__":
 
